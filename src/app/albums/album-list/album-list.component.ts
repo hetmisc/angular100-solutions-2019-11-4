d4aed145d90370922e03df9761979322c1c676d7
Judy Lipinki <judy@karmoxie.com>
2019-07-20 21:35:27 -0400

    updated to 8.1.2

---
+++ src/app/albums/album-list/album-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+
+import { AlbumService } from "../shared/album.service"
+import { Album } from "../album.model";
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: "app-album-list",
+  templateUrl: "./album-list.component.html",
+  styleUrls: ["./album-list.component.css"]
+})
+export class AlbumListComponent implements OnInit {
+  albumsArray: Observable<Album[]>;
+
+  constructor(private albumService: AlbumService) { }
+
+  ngOnInit(): void {
+    this.getAlbums();
+   }
+
+  parentFunctionHandler(album) {
+    alert('Album ' + album.albumName + ' was sent from the album card component');
+  }
+  
+  getAlbums() {
+    this.albumsArray = this.albumService.getAlbums();
+  }
+}
