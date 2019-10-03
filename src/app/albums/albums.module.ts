ef8e2af186ae7d076ee70551f074ff5045854b59
Judy Lipinski <judy@karmoxie.com>
2019-10-03 05:32:07 -0400

    have id in albums routing

---
+++ src/app/albums/albums.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlbumListComponent } from "./album-list/album-list.component";
+import { AlbumCardComponent } from "./album-card/album-card.component";
+import { AlbumsRoutingModule } from './albums-routing.module';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [ AlbumListComponent,
+    AlbumCardComponent,
+    AlbumDetailsComponent],
+  imports: [
+    CommonModule, AlbumsRoutingModule
+    , RouterModule
+  ]
+})
+export class AlbumsModule { }
